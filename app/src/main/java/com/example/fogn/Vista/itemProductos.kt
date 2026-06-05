928842f9096b06d4b7d407a6ed847b10915286f2
Usuario <ydielrn89@gmail.com>
2026-06-05 14:43:23 -0600

    1

---
+++ app/src/main/java/com/example/fogn/Vista/itemProductos.kt
@@ -0,0 +1,23 @@
+package com.example.fogn.Vista
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.fogn.R
+
+
+@Composable
+fun itemProductos (){
+
+    Row()
+    {
+        Image(painter= painterResource(R.drawable.baseline_image_24),contentDescription="Imagen")
+        Spacer(modifier=Modifier.width(30.dp))
+    }
+}
